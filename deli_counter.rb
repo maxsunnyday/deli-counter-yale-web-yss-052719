f8d71c688a9bcbd28fe88b2c74b43d87e1f29086
Max Sun <max.sun@yale.edu>
2019-05-16 19:37:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,10 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    print "The line is currently:"
-    #array.each do |person|
-    #  print "hi"
-    #end
+    puts "The line is currently: #{array}"
+    array.each do |person|
+
+    end
   end
 end
 
